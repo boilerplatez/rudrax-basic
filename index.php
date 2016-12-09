e561f1faad11382f2b666225c07a25fe94ddb150
LNT <lalit.tanwar07@gmail.com>
2016-12-09 13:21:20 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+ini_set('display_errors', 0);
+ini_set('error_reporting', 0);
+error_reporting(0);
+//error_reporting(E_ALL & ~E_DEPRECATED);
+
+require("./lib/autoload.php");
+require_once("./lib/rudrax/boot/RudraX.php");
+
+function controller_exception_handler($exception) {
+    echo "Uncaught exception: " , $exception->getMessage(), "\n";
+}
+
+try {
+    RudraX::invoke(array(
+        'RX_MODE_MAGIC' => TRUE,
+        'RX_MODE_DEBUG' => FALSE,
+        'PROJECT_ROOT_DIR' => __DIR__."/"
+    ));
+} catch(Exception $e){
+    controller_exception_handler($e);
+}
